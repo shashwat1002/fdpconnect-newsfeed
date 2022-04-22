ddaf649fc5cbaf6733db2311e2097e5fd046ca0f
Shashwat Singh <singhshashwat1002@gmail.com>
2022-04-22 20:04:10 +0530

    add setting.py and make scraping a full blown package

---
+++ src/scraping/__init__.py
@@ -0,0 +1,1 @@
+from .google_news_sitemap import *
---
+++ src/settings.py
@@ -0,0 +1,9 @@
+SAVE_ROOT_DIR = "model_vectorizer"
+CLASSIFIER_FILE_NAME = "classifier.sav"
+VECTORIZER_FILE_NAME = "vectorizer.sav"
+SCRAPED_DATA = "scraped.csv"
+
+SITEMAP_URLS = [
+    "https://www.business-standard.com/sitemap/today-google-news-sitemap.xml",
+    
+]
